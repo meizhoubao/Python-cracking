3c2111f9f3eb2ea5bb4e94b96f85e4bcbf2334fe
meizhoubao <18032773381@163.com>
2015-08-20 22:20:08 +0800

    Finally,the second way to try binascii module turned out to be right

--- crack_2.py
+++ crack_2.py
@@ -2,6 +2,7 @@
 # challenge 1:Convert hex to base64
 
 import base64
+import binascii
 
 hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
@@ -19,6 +20,11 @@ print ("try first way:")
 print (base64.b64encode(base64_string.encode(encoding="utf-8")))
 print (base64.urlsafe_b64encode(base64_string.encode(encoding="utf-8")))
 print (base64.standard_b64encode(base64_string.encode(encoding="utf-8")))
+print ("the first way is not right!")
 
-
-
+# binary_string = binascii.a2b_uu(base64_string)
+print ("try second way:")
+# print (base64.b64encode(binary_string))
+binary_string = binascii.a2b_hex(hex_string)
+print (base64.b64encode(binary_string))
+print ("OK,we got the correct answer!")
