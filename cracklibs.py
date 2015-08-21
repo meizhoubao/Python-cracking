dadbc98480900213375b17544ed4158f03b952a2
meizhoubao <18032773381@163.com>
2015-08-21 21:51:24 +0800

    Add function hex_xor()

--- cracklibs.py
+++ cracklibs.py
@@ -12,6 +12,10 @@ def convert_hex_to_base64(hex_string):
     binary_string = binascii.a2b_hex(hex_string)
     return base64.b64encode(binary_string)
 
-    
+# Fixed XOR
+def hex_xor(hex_string1,hex_string2):
+    int1 = int(hex_string1,16)
+    int2 = int(hex_string2,16)
+    return hex(int1 ^ int2)[2:]
 
 
