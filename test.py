23298a564ef66799ee72a174387a7307b83b922d
Leopard Li <18032773381@163.com>
2017-02-05 19:46:54 +0800

    Update and start new

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# The cryptopals crypto challenges
+
+from cracklibs import *
+
+# Crypto Challenge Set 1
+
+# Convert hex to base64
+hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+print("********** 1-1 Convert hex to base64 **********")
+print(convert_hex_to_base64(hex_string))
